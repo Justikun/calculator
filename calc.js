68cfbe6ef46b449642b622cd91c780beef832c0f
Justin Lowry <github@lowry.life>
2021-10-12 15:11:15 -0600

    Calc functional with + - * / √

--- calc.js
+++ calc.js
@@ -5,8 +5,6 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-
-
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
 	
@@ -14,8 +12,26 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
 
 	// This line closes the connection to the command line interface.
 	reader.close()
+	let solution;
+
+	if (mathSymbol === '+') {
+		solution = num1 + num2 
+	} else if (mathSymbol === '-') {
+		solution = num1 - num2
+	} else if (mathSymbol === '*') {
+		solution = num1 * num2
+	} else if (mathSymbol === '/') {
+		solution = num1 / num2
+	} else if (mathSymbol === '√') {
+		solution = Math.sqrt(num1, num2)
+	}
+
+	console.log("solution", solution)
 
 });
